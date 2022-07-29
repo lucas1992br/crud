3a130a800cfd53cb4da874336638a69af2d94ba9
Lucas Willian <pkk.master@gmail.com>
2022-07-29 11:00:50 -0300

    Layouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,67 +1,59 @@
-@extends('layouts.app')
+@extends('layouts.layout_login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Plusfin</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>E-mail ou Senha incorretos</strong>
-                                    </span>
-                                @enderror
-                            </div>
+<div class="login-box">
+    <!-- /.login-logo -->
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center">
+        <a href="" class="h1">Plusfin</a>
+        </div>
+        <div class="card-body">
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <div class="input-group mb-3">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>E-mail ou Senha incorretos</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                    </div>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>E-mail ou Senha incorretos</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="input-group mb-3">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Senha">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                             <span class="fas fa-lock"></span>
                         </div>
+                    </div>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>E-mail ou Senha incorretos</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="row">
+            <div class="col-8">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <label class="form-check-label" for="remember">
+                        {{ __('Lembrar-me') }}
+                    </label>
                 </div>
             </div>
-        </div>
+            <!-- /.col -->
+            <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">{{ __('Login') }}</button>
+            </div>
+            <!-- /.col -->
+            </div>
+        </form>
     </div>
-</div>
+    <!-- /.card -->
+    </div>
 @endsection
